d215bc8f2653cf9980df309bbc36fae43e586384
Erick <112605408+Ep1ore@users.noreply.github.com>
2024-07-11 18:54:52 -0600

    Can add products to database

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,94 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.REACT_APP_MONGODB_URI);
+
+// API Creation
+app.get('/', (req, res) => {
+    res.send('Express app is running');
+});
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+const upload = multer({storage:storage});
+
+// Creating upload endpoint for images
+app.use('/images', express.static('upload/images'));
+
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+// Schema for Creating Products
+const Product = mongoose.model('Product', {
+  id: {
+    type: Number,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  category: {
+    type: String,
+    require: true,
+  },
+  new_price: {
+    type: Number,
+    require: true,
+  },
+  old_price: {
+    type: Number,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+app.post('/addproduct', async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0){
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  console.log(product);
+  await product.save();
+  console.log('Saved');
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+});
 
 app.listen(port, (error) => {
     if (!error) {
