7e1b79ad5d4a1ced905da19ebf62719380841063
Erick <112605408+ErickAC12@users.noreply.github.com>
2024-07-21 03:44:38 -0600

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -6,7 +6,7 @@ const NewCollections = () => {
     const [new_collection, setNew_collection] = useState([]);
     
     useEffect(() => {
-        fetch('http://localhost:4000/newcollections')
+        fetch('https://e-commerce-backend-2ocm.onrender.com/newcollections')
         .then((res)=>res.json())
         .then((data)=>{setNew_collection(data)})
     },[]);
